030ee5ef8a60371849b5d1630aa40a5b4bc210d6
ios3109 <ios3109@Mac-mini-de-Caelum-70.local>
2012-11-28 10:03:35 -0200

    Initial Commit

---
+++ ContatosIP67/BSGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BSGAppDelegate.h
+//  ContatosIP67
+//
+//  Created by ios3109 on 28/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ContatosIP67/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ContatosIP67
+//
+//  Created by ios3109 on 28/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BSGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BSGAppDelegate class]));
+    }
+}
